79485035e6551b9acd223d6e7f28b7b565cd24b7
Coffe Cat <nikitatyshchenkoofficial@gmail.com>
2024-02-29 21:58:33 +0200

    Write json_-file name and root name through command line

---
+++ README.md
@@ -0,0 +1,14 @@
+**M/S FULL-STACK  DEV - test**
+
+This program converts json to xml.
+
+**Copy code**
+```
+git clone https://github.com/NikitaTyshcneko/TestTaskJsonToXml.git
+cd test_task_fama_group
+```
+
+**Run program**
+```
+python main.py your_json_file_name --root "your_root_name"
+```
--- main.py
+++ main.py
@@ -1,28 +1,29 @@
+import argparse
 import json
 import xml.etree.ElementTree as ElementTree
 from xml.dom import minidom
 
 
-def dict_to_xml(root_name: str, data: dict, indent: int = 0):
+def dict_to_xml(root_name: str, data: dict):
     root = ElementTree.Element(root_name)
     for key, value in sorted(data.items()):
-        elements_conditions(root, key, value, indent)
+        elements_conditions(root, key, value)
     return root
 
 
-def elements_conditions(root, key, value, indent):
+def elements_conditions(root, key, value):
     if isinstance(value, dict):
-        root.append(dict_to_xml(key, value, indent + 1))
+        root.append(dict_to_xml(key, value))
     elif isinstance(value, list):
-        add_list_value(root, key, value, indent)
+        add_list_value(root, key, value)
     else:
         root.append(create_element(key, value))
 
 
-def add_list_value(root: ElementTree.Element, key, value, indent: int):
+def add_list_value(root: ElementTree.Element, key, value):
     elements_tag = ElementTree.Element(key)
     for val in value:
-        elements_conditions(elements_tag, 'element', val, indent)
+        elements_conditions(elements_tag, 'element', val)
     root.append(elements_tag)
 
 
@@ -32,18 +33,23 @@ def create_element(element_name, value) -> ElementTree.Element:
     return element
 
 
-def prettify_xml(elem, indent="   "):
+def prettify_xml(elem):
     rough_string = ElementTree.tostring(elem, 'utf-8')
     reparsed = minidom.parseString(rough_string)
-    return reparsed.toprettyxml(indent=indent)
+    return reparsed.toprettyxml()
 
 
 def main():
-    with open('data.json') as json_file:
+    parser = argparse.ArgumentParser(description='Convert JSON to XML')
+    parser.add_argument('filename', help='JSON file to convert')
+    parser.add_argument('--rootname', help='Name for the root element', default='root')
+    args = parser.parse_args()
+
+    with open(args.filename) as json_file:
         json_data = json.load(json_file)
 
-    root_element = dict_to_xml("root", json_data)
-    xml_string = prettify_xml(root_element, indent="   ")
+    root_element = dict_to_xml(args.rootname, json_data)
+    xml_string = prettify_xml(root_element)
 
     print(xml_string)
 
